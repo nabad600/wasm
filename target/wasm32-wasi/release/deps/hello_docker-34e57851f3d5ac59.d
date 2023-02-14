7816a9fb66b465b0339cd4af30895aa00fa62923
Naba Das <nabad600@gmail.com>
2023-02-14 12:27:42 +0530

    image create

---
+++ target/wasm32-wasi/release/deps/hello_docker-34e57851f3d5ac59.d
@@ -0,0 +1,5 @@
+/home/ubuntu/hello-docker/target/wasm32-wasi/release/deps/hello_docker-34e57851f3d5ac59.wasm: src/main.rs
+
+/home/ubuntu/hello-docker/target/wasm32-wasi/release/deps/hello_docker-34e57851f3d5ac59.d: src/main.rs
+
+src/main.rs:
---
+++ target/wasm32-wasi/release/hello-docker.d
@@ -0,0 +1,1 @@
+/home/ubuntu/hello-docker/target/wasm32-wasi/release/hello-docker.wasm: /home/ubuntu/hello-docker/src/main.rs
